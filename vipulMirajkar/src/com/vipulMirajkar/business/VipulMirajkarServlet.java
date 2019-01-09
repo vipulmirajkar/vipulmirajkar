b16818c0ebd209d60001390f7262b238a05755ca
Vipul Developer <Vipul Developer@DESKTOP-U213JD6>
2019-01-09 19:50:04 +0530

    First Commit
    Uploading All Files

---
+++ vipulMirajkar/src/com/vipulMirajkar/business/VipulMirajkarServlet.java
@@ -0,0 +1,11 @@
+package com.vipulMirajkar.business;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class VipulMirajkarServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+	}
+}
